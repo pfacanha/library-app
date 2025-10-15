2878d2bdad217e7007fb48143f5662bcf640241e
pfacanha <pfacanha.dev@gmail.com>
2025-10-14 23:12:54 -0700

    Check display function implementation

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
-const myLibrary = ["Harry Potter", "Lord of Rings", "Heart of Darkness"];
-
-function Books(title, author) {
+function Book(title, author) {
   const id = crypto.randomUUID();
   this.id = id;
   this.title = title;
   this.author = author;
 }
+const book1 = new Book("Harry Potter", "J.K Rowling");
+const book2 = new Book("Lord of The Rings", "J.R.R. Tolkien");
+const book3 = new Book("Lord of The Rings II", "J.R.R. Tolkien");
+
+const myLibrary = [book1, book2, book3];
+const tableContainer = document.querySelector("table-container");
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-function displayBooks(array, list) {
-  return array.forEach((listText) => {
-    const listItem = document.createElement("li");
+function displayBooks(array, table) {
+  return array.forEach((book) => {
+    const newRow = document.createElement("tr");
+    const id = document.createElement("td");
+    id.textContent = book.id;
 
-    listItem.textContent = listText;
+    const movieTitle = document.createElement("td");
+    movieTitle.textContent = book.title;
 
-    list.appendChild(listItem);
+    const authorTitle = document.createElement("td");
+    authorTitle.textContent = book.author;
+
+    newRow.appendChild(movieTitle);
+    newRow.appendChild(authorTitle);
+
+    table.appendChild(newRow);
   });
 }
 
 const myList = document.querySelector(".book-list");
-
-displayBooks(myLibrary, myList);
+displayBooks(myLibrary, tableContainer);
