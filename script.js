9e07bce5a2aab0fb146653269816089323ab800e
pfacanha <pfacanha.dev@gmail.com>
2025-12-07 19:45:12 -0800

    Display books and got a decent restart for the project

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const viewBooks = document.querySelector(".view");
 
 function Book(title, author, isRead) {
   this.id = crypto.randomUUID();
@@ -7,14 +8,35 @@ function Book(title, author, isRead) {
   this.isRead = isRead ? "read" : "not read yet";
 }
 
-function addBookToLibrary(title, author) {
-  const book = new Book(title, author);
+function addBookToLibrary(title, author, isRead) {
+  const book = new Book(title, author, isRead);
   myLibrary.push(book);
   return book;
 }
 
 addBookToLibrary("Harry Potter", "J.K. Rowling", false);
+addBookToLibrary("Harry Potter II", "J.K. Rowling", true);
 
-console.log(myLibrary);
-console.log(Object.getPrototypeOf(myLibrary));
-console.log(Book.prototype);
+function displayBooks() {
+  for (let i = 0; i < myLibrary.length; ++i) {
+    let curr = myLibrary[i];
+    const currBook = document.createElement("div");
+
+    const author = document.createElement("p");
+    author.textContent = `Author: ${curr.author}`;
+
+    const title = document.createElement("p");
+    title.textContent = `Title: ${curr.title}`;
+
+    const isRead = document.createElement("p");
+    isRead.textContent = `isRead: ${curr.isRead}`;
+
+    const line = document.createElement("p");
+    line.textContent = `-----------------`;
+
+    currBook.append(author, title, isRead);
+    viewBooks.append(currBook, line);
+  }
+}
+
+displayBooks();
