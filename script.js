de64f6294498be77186e011b0692a2591f0f6336
pfacanha <pfacanha.dev@gmail.com>
2025-12-08 10:15:34 -0800

    Add classlist .card to each element created inside displayBooks()

--- script.js
+++ script.js
@@ -35,6 +35,8 @@ function displayBooks() {
     const line = document.createElement("p");
     line.textContent = `-----------------`;
 
+    currBook.classList = "card";
+
     currBook.append(author, title, isRead);
     viewBooks.append(currBook, line);
   }
